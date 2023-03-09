319152d364d2ab27480be06f255b4913b8b2ffc2
Omar <account1@fulls-mbp.attlocal.net>
2023-03-08 22:32:13 -0800

    Added recoil package

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Screens/login";
 import Dashboard from "./Screens/dashboard";
 import AdminDashboard from "./Screens/admin-dashboard";
+import { RecoilRoot } from "recoil";
 import "./App.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="admin-dashboard" element={<AdminDashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="admin-dashboard" element={<AdminDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 };
 
